ab30eeb4571916d19e68d603d0a6b8e67e4db4d6
miao <chuoli223@yahoo.com>
2025-04-25 18:35:49 +0200

    remove numerosity filter in select_displays

--- src/select_displays/select_displays.R
+++ src/select_displays/select_displays.R
@@ -1,5 +1,4 @@
-#从生成的原始displays中，选出需要的sector_angle和direction。每一个条件下只保留
-#需要的nuemrosity（这些是numerosity分布中出现得最多的几个）
+#从生成的原始displays中，选出需要的sector_angle和direction。
 
 setwd("d:/OneDrive/projects/numerosity_closing_gap/displays/displays_n5000/")
 
@@ -7,23 +6,23 @@ folder_path <- "d:/OneDrive/projects/numerosity_closing_gap/displays/displays_n5
 
 # Parameters to filter (can be vectors)
 arrangement <- c("radial", "tangential")  
-sector_angle <- c(30, 60, 90, 120, 170)                
+sector_angle <- c(30, 40, 60, 90, 120, 170)                
 direction <- c(0, 180)    
 
-# Allowed numerosity per sector angle
-numerosity_filter <- list(
-  `30` = c(6, 9, 12, 15),
-  `60` = c(12, 15, 18, 9),
-  `90` = c(18, 21, 24, 15, 27),
-  `120` = c(24, 27, 21, 30, 18),
-  `170` = c(33, 36, 30, 39, 27)
-)
+# # Allowed numerosity per sector angle
+# numerosity_filter <- list(
+#   `30` = c(6, 9, 12, 15),
+#   `60` = c(12, 15, 18, 9),
+#   `90` = c(18, 21, 24, 15, 27),
+#   `120` = c(24, 27, 21, 30, 18),
+#   `170` = c(33, 36, 30, 39, 27)
+# )
 
 # List all csv files in the folder
 all_files <- list.files(path = folder_path, pattern = "\\.csv$", full.names = TRUE)
 
 # Initialize matched files
-matched_files <- c
+matched_files <- c()
 
 # Check all combinations of A, B, C
 for (A in arrangement) {
@@ -57,12 +56,12 @@ data_list <- lapply(matched_files, function(file_path) {
   df$sector_angle <- angle
   df$visual_field <- direction
   
-  # Filter numerosity based on sector_angle
-  if (as.character(angle) %in% names(numerosity_filter)) {
-    allowed_values <- numerosity_filter[[as.character(angle)]]
-    df <- df[df$numerosity %in% allowed_values, ]
-  }
-  
+  # # Filter numerosity based on sector_angle
+  # if (as.character(angle) %in% names(numerosity_filter)) {
+  #   allowed_values <- numerosity_filter[[as.character(angle)]]
+  #   df <- df[df$numerosity %in% allowed_values, ]
+  # }
+  # 
   # Save to new location
   save_path <- file.path(save_folder, file_name)
   write.csv(df, save_path, row.names = FALSE)
