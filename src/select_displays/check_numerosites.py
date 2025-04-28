15ea5293089d823f37923b19c484450ecd56c575
miao <chuoli223@yahoo.com>
2025-04-28 17:13:42 +0200

    filter non-used numerosity before matching properties

--- src/select_displays/check_numerosites.py
+++ src/select_displays/check_numerosites.py
@@ -22,3 +22,39 @@ pivot = pd.pivot_table(
     aggfunc='count',
     fill_value=0
 )
+
+# remove displays if numerosity cannot be used
+all_displays = all_displays[all_displays['sector_angle'].isin([40, 60, 90, 120, 170])]
+
+
+# set the filter - based on sector_angle and numerosity
+filter = (
+    # For sector_angle 40, keep numerosity 5-17
+        ((all_displays['sector_angle'] == 40) &
+         (all_displays['numerosity'] >= 5) &
+         (all_displays['numerosity'] <= 13)) |
+
+    # For sector_angle 60, keep numerosity 8-17
+        ((all_displays['sector_angle'] == 60) &
+         (all_displays['numerosity'] >= 8) &
+         (all_displays['numerosity'] <= 17)) |
+
+
+        # For sector_angle 90, keep numerosity 13-22
+        ((all_displays['sector_angle'] == 90) &
+         (all_displays['numerosity'] >= 13) &
+         (all_displays['numerosity'] <= 22)) |
+
+        # For sector_angle 120, keep numerosity 18-28
+        ((all_displays['sector_angle'] == 120) &
+         (all_displays['numerosity'] >= 18) &
+         (all_displays['numerosity'] <= 28)) |
+
+        # For sector_angle 170, keep numerosity 25-39
+        ((all_displays['sector_angle'] == 170) &
+         (all_displays['numerosity'] >= 25) &
+         (all_displays['numerosity'] <= 39))
+)
+
+# Apply the filter
+all_displays = all_displays[filter]
